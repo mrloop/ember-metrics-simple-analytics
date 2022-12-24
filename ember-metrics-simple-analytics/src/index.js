4d27786f421a3730df249c13df1fe504f8c74d50
mrloop <ewan@mrloop.com>
2022-12-24 15:34:27 +0000

    Add SimpleAnalytics adatper for ember-metrics

---
+++ ember-metrics-simple-analytics/src/index.js
@@ -0,0 +1,66 @@
+import { isPresent } from '@ember/utils';
+import { camelize } from '@ember/string';
+import BaseAdapter from 'ember-metrics/metrics-adapters/base';
+
+export default class SimpleAnalytics extends BaseAdapter {
+  toStringExtension() {
+    return 'SimpleAnalytics';
+  }
+
+  install() {
+    const config = { autoCollect: false, ...this.config };
+    this._injectScript(config);
+  }
+
+  _injectScript(config) {
+    this._script = document.createElement('script');
+    this._script.src = 'https://scripts.simpleanalyticscdn.com/latest.js';
+    Object.entries(config).forEach(([key, value]) => {
+      if (isPresent(value)) this._script.dataset[camelize(key)] = String(value);
+    });
+    document.body.appendChild(this._script);
+  }
+
+  identify() {
+    // noop
+  }
+
+  trackEvent(options = {}) {
+    let { name, ...metadata } = options;
+    window[this._namespaceEvent](name, metadata);
+  }
+
+  trackPage(options = {}) {
+    let { path, ...metadata } = options;
+    window[this._namespacePageview](path, metadata);
+  }
+
+  uninstall() {
+    document.body.removeChild(this._script);
+    delete window[this._namespaceEvent];
+    delete window[this._namespacePageview];
+    delete window[this._namespaceLoaded];
+    delete window[this._namespaceMetadata];
+  }
+
+  // https://docs.simpleanalytics.com/events#the-variable-sa_event-is-already-used
+  get _namespace() {
+    return this.config?.namespace || 'sa';
+  }
+
+  get _namespaceEvent() {
+    return `${this._namespace}_event`;
+  }
+
+  get _namespacePageview() {
+    return `${this._namespace}_pageview`;
+  }
+
+  get _namespaceLoaded() {
+    return `${this._namespace}_loaded`;
+  }
+
+  get _namespaceMetadata() {
+    return `${this._namespace}_metadata`;
+  }
+}
---
+++ test-app/tests/unit/ember-metrics-simple-analytics-adapter-test.js
@@ -0,0 +1,97 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import SimpleAnalytics from 'ember-metrics-simple-analytics';
+
+module('Unit | Adapter | simple-analytics', function (hooks) {
+  setupTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.adapter = new SimpleAnalytics();
+  });
+
+  hooks.afterEach(function () {
+    if (scriptElement()) this.adapter.uninstall();
+  });
+
+  function scriptElement() {
+    return document.querySelector('[src*="simpleanalytics"]');
+  }
+
+  module('install data-attributes', function () {
+    test('script data attributes can be configured', function (assert) {
+      this.adapter.config = {
+        collectDnt: false,
+        hostname: 'picturewham.com',
+        ignoreMetrics: ['timeonpage', 'scrolled'],
+      };
+      this.adapter.install();
+      assert.strictEqual(scriptElement().dataset.collectDnt, 'false');
+      assert.strictEqual(scriptElement().dataset.hostname, 'picturewham.com');
+      assert.strictEqual(
+        scriptElement().dataset.ignoreMetrics,
+        'timeonpage,scrolled'
+      );
+    });
+
+    test('it defaults auto collect to false', function (assert) {
+      this.adapter.install();
+      assert.strictEqual(scriptElement().dataset.autoCollect, 'false');
+    });
+
+    test('the default auto collect can be overridden', function (assert) {
+      this.adapter.config = { autoCollect: true };
+      this.adapter.install();
+      assert.strictEqual(scriptElement().dataset.autoCollect, 'true');
+    });
+  });
+
+  module('uninstall', function () {
+    test('it removes the script element', function (assert) {
+      this.adapter.install();
+      assert.true(scriptElement() instanceof HTMLScriptElement);
+
+      this.adapter.uninstall();
+      assert.strictEqual(scriptElement(), null);
+    });
+  });
+
+  module('function calls', function () {
+    test('#trackEvent calls simpleanalytics with the correct arguments', function (assert) {
+      this.adapter.install();
+      assert.expect(1);
+      window.sa_event = (...args) =>
+        assert.deepEqual(args, ['test', { plan: 'starter' }]);
+      this.adapter.trackEvent({ name: 'test', plan: 'starter' });
+    });
+
+    test('#trackPage calls simpleanalytics with the correct arguments', function (assert) {
+      this.adapter.install();
+      assert.expect(1);
+      window.sa_pageview = (...args) =>
+        assert.deepEqual(args, ['/test', { plan: 'starter' }]);
+      this.adapter.trackPage({ path: '/test', plan: 'starter' });
+    });
+
+    module('configure namespace', function (hooks) {
+      hooks.beforeEach(function () {
+        this.adapter.config = { namespace: 'mrloop' };
+      });
+
+      test('#trackEvent calls simpleanalytics with the correct arguments', function (assert) {
+        this.adapter.install();
+        assert.expect(1);
+        window.mrloop_event = (...args) =>
+          assert.deepEqual(args, ['test', { plan: 'starter' }]);
+        this.adapter.trackEvent({ name: 'test', plan: 'starter' });
+      });
+
+      test('#trackPage calls simpleanalytics with the correct arguments', function (assert) {
+        this.adapter.install();
+        assert.expect(1);
+        window.mrloop_pageview = (...args) =>
+          assert.deepEqual(args, ['/test', { plan: 'starter' }]);
+        this.adapter.trackPage({ path: '/test', plan: 'starter' });
+      });
+    });
+  });
+});
